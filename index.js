abca0e8f6340f9f989409968b0adc4a40d9f508d
Sushant7975 <skkhot29@gmail.com>
2024-05-11 12:57:49 +0530

    Some changes done

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = require('./src/app.js')
 const mongoose = require('mongoose')
 const port = 3000
 
+// const subscriberModel = require('./src/models/subscribers.js');
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-
-
 
 // Connect to DATABASE
 const mongoURL= "mongodb://127.0.0.1:27017/users"
@@ -16,6 +16,10 @@ mongoose.connect(mongoURL, /*{ useNewUrlParser: true, useUnifiedTopology: true }
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('Connected to database'))
+
+//Import the router files
+const subscribersRoute = require('./src/routes/subscribersRoute.js');
+app.use('/subscribers', subscribersRoute);
 
 // Start Server
 app.listen(port, () => console.log(`App listening on port ${port}!`))
