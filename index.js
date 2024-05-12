82bbae8d7686c78cd79793536e4c4ba5bd41fba8
Sushant7975 <skkhot29@gmail.com>
2024-05-12 13:16:59 +0530

    Some changes done

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const express = require('express')
-const app = require('./src/app.js')
-const mongoose = require('mongoose')
-require('dotenv').config();
+const express = require("express");
+const app = require("./src/app.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // const subscriberModel = require('./src/models/subscribers.js');
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
 //const mongoURL = process.env.MONGODB_URL_LOCAL;
 const DATABASE_URL = process.env.MONGODB_URL;
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('Connected to database'))
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to database"));
 
 //Import the router files
-const subscribersRoute = require('./src/routes/subscribersRoute.js');
-app.use('/subscribers', subscribersRoute);
+const subscribersRoute = require("./src/routes/subscribersRoute.js");
+app.use("/subscribers", subscribersRoute);
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
--- src/app.js
+++ src/app.js
@@ -10,11 +10,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-
-
-// Handles all the unwanted requests.
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
-
 module.exports = app;
