b8b1db37352901f2feb0b7abda47c1318a417f03
Sushant7975 <skkhot29@gmail.com>
2024-05-14 00:12:40 +0530

    changes done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
 // const mongoURL = process.env.MONGODB_URL_LOCAL;
-const DATABASE_URL = "mongodb+srv://skkhot29:skkhot4575@subscribers.skktd3y.mongodb.net/" ;
+const DATABASE_URL = process.env.MONGODB_URL_ATLAS;
 
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
